6279ea846d841c0dba667521463c19e3769a3652
Eric Chen <eric@Erics-MacBook-Pro-2.local>
2014-10-18 23:57:36 -0400

    killed slider

--- buttonmake.js
+++ buttonmake.js
@@ -8,11 +8,13 @@ for(var i=0; i<5; i++){
       btn.innerHTML="Pause";
       btn.setAttribute("onclick","myFunction()");
       document.body.appendChild(btn);
-      
-      var slider = new Slider('#ex1', {
-	formatter: function(value) {
-		return 'Current value: ' + value;
-	}
-});
       document.write("<BR/>");
-  }
+
+      /*var slider = new Slider('#ex1', {
+	    formatter: function(value) {
+		  return 'Current value: ' + value;
+      
+	}
+  } */
+}
+
